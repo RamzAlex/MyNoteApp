932600bd3f5205bb317bfa3b3f07a595608c49b4
Alex Ramirez <jesus.ramirez@alumnos.udg.mx>
2020-12-05 12:42:57 -0600

    Initial commit

---
+++ app/src/main/java/mx/udg/cuvalles/mynoteapp/NotaDao.kt
@@ -0,0 +1,22 @@
+package mx.udg.cuvalles.mynoteapp
+
+import androidx.room.*
+
+@Dao
+interface NotaDao {
+
+    @Query("select * from Nota")
+    suspend fun getAll():List<Nota>
+
+    @Query("select * from Nota where id = :id")
+    suspend fun getById(id:Int):Nota
+
+    @Update
+    suspend fun Actualizar(nota: Nota)
+
+    @Insert
+    suspend fun insert(notas: List<Nota>)
+
+    @Delete
+    suspend fun eliminar(nota: Nota)
+}
---
+++ app/src/main/java/mx/udg/cuvalles/mynoteapp/NotasDb.kt
@@ -0,0 +1,13 @@
+package mx.udg.cuvalles.mynoteapp
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(
+        entities = [Nota::class],
+        version = 1
+)
+abstract class NotasDb: RoomDatabase(){
+        abstract fun notaDao():NotaDao
+
+}
